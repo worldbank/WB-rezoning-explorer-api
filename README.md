8daaaf9d89ee8b24b0599c8d24dbc6f57cf84b3a
Drew Bollinger <drew@developmentseed.org>
2020-09-05 19:27:53 -0400

    update demo, readme, and docs

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-#rezoning-api
+# rezoning-api
+
+Reneweable Energy Zone Calculations. Backend to https://github.com/developmentseed/rezoning-web
+
+Deployed at https://cb1d9tl7ve.execute-api.us-east-2.amazonaws.com/
+
+Documentation at https://cb1d9tl7ve.execute-api.us-east-2.amazonaws.com/docs
+
+## Primary functions
+
+This API exposes two primary functions:
+1. A tile server to display areas which satisfy a set of geospatial filters (e.g. areas within 5000 meters of a road and 10000 meters away from transmission lines)
+2. The ability to calculate "zone statistics" for a given area of interest, including Levelized Cost of Energy (LCOE) and "zone score" based on user provided weights
+
+### Tile Server
+
+![demonstration of the tile server of Africa](images/rezoning-api-filter.gif)
+
+The tile server is available as an XYZ endpoint at: `/v1/filter/{z}/{x}/{y}.png`. It further accepts two path parameters:
+
+`filter=0,1000|500,1000|...`: a pipe separated list of distance ranges in meters (e.g. `0,5000`) from a given feature. The list of distance layers is available at `/v1/filter/layers`
+
+`color=45,39,88,178`: a comma separated list of RGBA values to display the filtered areas as. All values, including opacity, are 0-255.
+
+A UI demonstration of this functionality is available at `/v1/demo`
+
+### Zone Statistics
+
+This function is relatively complicated but has sensible defaults. A rough summary of the process:
+1. User submits an area of interest (AOI) as a [GeoJSON](https://geojson.org/) `Polygon` and a set of filters matching the above format, along with optional parameters to override defaults.
+2. 
--- rezoning_api/models/zone.py
+++ rezoning_api/models/zone.py
@@ -1,37 +1,41 @@
 """LCOE models"""
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from geojson_pydantic.geometries import Polygon
+
+def WeightField(title=None):
+    return Field(0.5, gt=0, lt=1, description=f'weight assigned to {title} parameter', title=title)
 
 
 class Weights(BaseModel):
-    lcoe_gen: float = 0.5
-    lcoe_transmission: float = 0.5
-    lcoe_road: float = 0.5
-    distance_load: float = 0.5
-    technology_colocation: float = 0.5
-    human_footprint: float = 0.5
-    pop_density: float = 0.5
-    slope: float = 0.5
-    land_use: float = 0.5
-    capacity_value: float = 0.5
+    Field()
+    lcoe_gen: float = WeightField(title="LCOE Generation")
+    lcoe_transmission: float = WeightField(title="LCOE Transmission")
+    lcoe_road: float = WeightField(title="LCOE Road")
+    distance_load: float = WeightField(title="Distance to Load")
+    technology_colocation: float = WeightField(title="Technology Colocation")
+    human_footprint: float = WeightField(title="Human Footprint")
+    pop_density: float = WeightField(title="Population Density")
+    slope: float = WeightField(title="Slope")
+    land_use: float = WeightField(title="Land Use Score")
+    capacity_value: float = WeightField(title="Capacity Value")
 
 class LCOE(BaseModel):
     """Levelized cost of energy request."""
 
-    turbine_type: Optional[int]  # Turbine Type or Solar Unit Type
-    crf: float = 1  # capital recovery factor (CRF)
-    cg: int = 2000  # Generation – capital [USD/kW] (Cg)
-    omfg: int = 50000  # Generation – fixed O&M [USD/MW/y] (OMf,g)
-    omvg: float = 4  # Generation – variable O&M [USD/MWh] (OMv,g)
-    ct: int = 990  # Transmission (land cabling) – capital [USD/MW/km] (Ct)
-    omft: int = 0  # Transmission – fixed O&M [USD/km] (OMf,t)
-    cs: float = 71000  # Substation – capital [USD / two substations (per new transmission connection) ] (Cs)
-    cr: float = 407000  # Road – capital [USD/km] (Cr)
-    omfr: float = 0  # Road – fixed O&M [USD/km] (OMf,r)
-    decom: float = 0  # Decommission % rate (Decom)
-    i: float = 0.1  # Economic discount rate (i)
-    n: float = 25  # Lifetime [years] (N)
+    turbine_type: Optional[int] = Field(None, title="Turbine Type or Solar Unit Type")
+    crf: float = Field(1, title="Capital Recovery Factor (CRF)")
+    cg: int = Field(2000, title="Generation – capital [USD/kW] (Cg)")
+    omfg: int = Field(50000, title="Generation – fixed O&M [USD/MW/y] (OMf,g)")
+    omvg: float = Field(4, title="Generation – variable O&M [USD/MWh] (OMv,g)")
+    ct: int = Field(990, title="Transmission (land cabling) – capital [USD/MW/km] (Ct)")
+    omft: int = Field(0, title="Transmission – fixed O&M [USD/km] (OMf,t)")
+    cs: float = Field(71000, title="Substation – capital [USD / two substations (per new transmission connection) ] (Cs)")
+    cr: float = Field(407000, title="Road – capital [USD/km] (Cr)")
+    omfr: float = Field(0, title="Road – fixed O&M [USD/km] (OMf,r)")
+    decom: float = Field(0, title="Decommission % rate (Decom)")
+    i: float = Field(0.1, title="Economic discount rate (i)")
+    n: float = Field(25, title="Lifetime [years] (N)")
 
 class ZoneRequest(BaseModel):
     aoi: Polygon
