2df661f6f0562040bc429a3b5b7276956f667d58
Drew Bollinger <drew@developmentseed.org>
2020-11-19 10:38:44 -0800

    broaden range filter regex

--- rezoning_api/models/zone.py
+++ rezoning_api/models/zone.py
@@ -4,7 +4,7 @@ from typing import Optional, Union
 from pydantic import BaseModel, Field
 from geojson_pydantic.geometries import Polygon, MultiPolygon
 
-range_filter_regex = re.compile(r"\d+,\d+")
+range_filter_regex = re.compile(r"[\d\.]+,[\d\.]+")
 categorical_filter_regex = re.compile(r"(\w+,)*\w+")
 
 
