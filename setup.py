3656750fea87d5aa2428288821227fdd260d6756
Drew Bollinger <drew@developmentseed.org>
2021-01-18 17:14:58 -0500

    version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ extra_reqs = {
 
 setup(
     name="rezoning-api",
-    version="0.1.49",
+    version="0.1.50",
     python_requires=">=3",
     description=u"""API for the REZoning project""",
     packages=find_packages(exclude=["tests"]),
