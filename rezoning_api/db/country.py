455a44385c9d84e70c8be8575c93e2b81448f186
Drew Bollinger <drew@developmentseed.org>
2021-05-12 22:28:32 -0400

    update demo, drop old lcoe obj

--- rezoning_api/db/country.py
+++ rezoning_api/db/country.py
@@ -92,7 +92,6 @@ def get_country_min_max(id):
             mm["max"] = mm["max"] * 365
 
     # replace lcoe object with hardcoded minmax
-    mm_obj["lcoe"]["min"] = 80
-    mm_obj["lcoe"]["max"] = 200
+    mm_obj["lcoe"] = dict(min=80, max=300)
 
     return mm_obj
