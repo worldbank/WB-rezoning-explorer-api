acc4863899cd9167cadafb87332845996da912fb
akshay2408 <akshaybabar2408@gmail.com>
2023-06-05 16:33:35 +0530

    Added status code

--- rezoning_api/api/api_v1/endpoints/feedback.py
+++ rezoning_api/api/api_v1/endpoints/feedback.py
@@ -7,7 +7,7 @@ from rezoning_api.core.config import FEEDBACK_URL, GITHUB_TOKEN
 router = APIRouter()
 
 
-@router.post("/feedback", description="Feedback submission")
+@router.post("/feedback", status_code=201, description="Feedback submission")
 async def feedback(request: Request):
     """This api creates a ticket in github repo, when user submits a feedback form."""
     data = await request.json()
