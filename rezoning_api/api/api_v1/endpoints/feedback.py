581167472707dfe7e99fff0825a575a98197d5f6
akshay2408 <akshaybabar2408@gmail.com>
2023-06-05 15:29:08 +0530

    Added feedback api

---
+++ rezoning_api/api/api_v1/endpoints/feedback.py
@@ -0,0 +1,15 @@
+"""Feedback endpoints"""
+import requests
+from fastapi import Request, APIRouter
+from rezoning_api.core.config import FEEDBACK_URL, GITHUB_TOKEN
+
+
+router = APIRouter()
+
+
+@router.post("/feedback", description="Feedback submission")
+async def feedback(request: Request):
+    """This api creates a ticket in github repo, when user submits a feedback form."""
+    data = await request.json()
+    response = requests.post(FEEDBACK_URL, headers={'authorization': GITHUB_TOKEN}, json=data)
+    return {"status": response.status_code}
